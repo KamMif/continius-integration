07fd4ea4b37634a99ad7db6c5e0f8604c90c1ff3
Kam <kammif1990@gmail.com>
2018-04-09 15:32:37 +0500

    add

--- sum/sum.js
+++ sum/sum.js
@@ -1,6 +1,5 @@
 function sum(x, y) {
   return x + y
-  console.log(1);
 };
 
 module.exports = sum;
--- sum/sum.test.js
+++ sum/sum.test.js
@@ -8,3 +8,6 @@ test('adds 1 + 2 to eqqual 3', () => {
   expect(sum(1, 2)).toBe(3)
 });
 
+test('adds 1 + 2 to eqqual 3', () => {
+  expect(sum(1, 2)).toBe(1)
+});
