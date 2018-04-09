ba7cae9a5890fad1d50369b83339078abf90939c
Kam <kammif1990@gmail.com>
2018-04-09 15:26:13 +0500

    as

--- sum/sum.test.js
+++ sum/sum.test.js
@@ -7,3 +7,7 @@ test('adds 1 + 2 to eqqual 3', () => {
 test('adds 1 + 2 to eqqual 3', () => {
   expect(sum(1, 2)).toBe(3)
 });
+
+test('adds 1 + 2 to eqqual 3', () => {
+  expect(sum(1, 2)).toBe(6)
+});
